9fbc53a394560d44bdbc3dce563eec642857a36f
saurabh30 <gupta.saurabh711@gmail.com>
2018-06-13 10:31:41 +0530

    made random classifier

--- pipeline.py
+++ pipeline.py
@@ -1,3 +1,19 @@
+import random
+
+class ScrappyKNN():
+	def fit(self, X_train, y_train):
+	    self.X_train = X_train
+	    self.y_train = y_train	
+
+	def predict(self, X_test):
+	    predictions = []
+	    for row in X_test:
+		label = random.choice(self.y_train)
+		predictions.append(label)
+	    return predictions				
+
+
+
 # import a dataset
 from sklearn import datasets
 iris = datasets.load_iris()
@@ -8,8 +24,8 @@ y = iris.target
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .5)
 
-from sklearn.neighbors import KNeighborsClassifier
-my_classifier = KNeighborsClassifier()
+#from sklearn.neighbors import KNeighborsClassifier
+my_classifier = ScrappyKNN()
 
 my_classifier.fit(X_train, y_train)
 
