9fb0bf7f2846ffe7be49d578d40c627d7594c85c
saurabh30 <gupta.saurabh711@gmail.com>
2018-06-14 12:18:55 +0530

    made nearest neighbour classifier

--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,9 @@
-import random
+from scipy.spatial import distance
 
+def euc(a,b) :
+    return distance.euclidean(a,b)
+
+	
 class ScrappyKNN():
 	def fit(self, X_train, y_train):
 	    self.X_train = X_train
@@ -8,9 +12,19 @@ class ScrappyKNN():
 	def predict(self, X_test):
 	    predictions = []
 	    for row in X_test:
-		label = random.choice(self.y_train)
+		label = self.closest(row)
 		predictions.append(label)
-	    return predictions				
+	    return predictions	
+	
+	def closest(self, row):
+	    best_dist = euc(row, self.X_train[0])
+	    best_index = 0
+	    for i in range(1, len(self.X_train)):
+		dist = euc(row, self.X_train[i])
+		if dist < best_dist:
+		    best_dist = dist
+		    best_index = i
+	    return self.y_train[best_index]							 			
 
 
 
